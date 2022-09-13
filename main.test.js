dbfb050cbc78fdcbeb78b36f25d032f8ea44f4c7
Son Do <dominhson580@gmail.com>
2022-09-13 17:02:19 +0700

    Create some tests for capitalize()

--- main.test.js
+++ main.test.js
@@ -5,3 +5,19 @@ import {
   caesarCipher,
   analyzeArray,
 } from "./main";
+
+test("Empty string", () => {
+  expect(capitalize("")).toBe("");
+});
+
+test("Non-alphabet characters", () => {
+  expect(capitalize("!duck")).toBe("!duck");
+});
+
+test("Already capitalize", () => {
+  expect(capitalize("Sonic")).toBe("Sonic");
+});
+
+test("Need capitalize", () => {
+  expect(capitalize("xZero")).toBe("XZero");
+});
