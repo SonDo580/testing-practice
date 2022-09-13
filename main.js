9cf7ae13b3f7a6050765514e5d2ffda30d9b3889
Son Do <dominhson580@gmail.com>
2022-09-13 16:41:38 +0700

    Create the functions and export them

---
+++ main.js
@@ -0,0 +1,14 @@
+function capitalize() {}
+function reverseString() {}
+
+const calculator = {
+  add() {},
+  subtract() {},
+  divide() {},
+  multiply() {},
+};
+
+function caesarCipher() {}
+function analyzeArray() {}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
