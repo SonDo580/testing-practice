091a5020dc73979697c25fdefaed81d53ce3acf6
Son Do <dominhson580@gmail.com>
2022-09-13 21:44:47 +0700

    Define divide()

--- main.js
+++ main.js
@@ -23,7 +23,13 @@ const calculator = {
   subtract(x, y) {
     return x - y;
   },
-  divide() {},
+  divide(x, y) {
+    if (y === 0) {
+      return NaN;
+    }
+
+    return x / y;
+  },
   multiply(x, y) {
     return x * y;
   },
